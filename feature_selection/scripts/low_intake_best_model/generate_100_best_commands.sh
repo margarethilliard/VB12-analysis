d231e2bfc9d5be4bfb5d6ff3a91c17501ab2a139
Margaret Hilliard <53877776+margarethilliard@users.noreply.github.com>
2025-10-02 15:45:28 -0400

    Add files via upload

---
+++ feature_selection/scripts/low_intake_best_model/generate_100_best_commands.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for seed in $(cat random_seeds.txt) 
+do
+echo "mkdir /quobyte/dglemaygrp/mhilliard/FL100/taxaHFE/gtdb_taxonomy/regression_models/low_intake_best_model/output_${seed} && singularity run --pwd /app --no-home --contain --workdir /quobyte/dglemaygrp/mhilliard/FL100/taxaHFE/gtdb_taxonomy/regression_models/low_intake_best_model/ --bind /quobyte/dglemaygrp/mhilliard/FL100/taxaHFE/gtdb_taxonomy/regression_models/low_intake_best_model/:/data /quobyte/dglemaygrp/aoliver/software/taxahfe_ml_dev.sif taxaHFE_metadata.csv modified_merged_metaphlan_v4-0-6_GTDB.txt -o output_${seed} -s subject_id -l plasma_b12 -t numeric -L 3 -wWD -n 6 --metric rmse --prevalence 0.25 --abundance 0.01 -c 0.95 --train_split 0.75 --tune_length 80 --tune_time 2 --tune_stop 10 --folds 5 --nperm 80 --seed ${seed} -d" >> 100_best_commands.txt
+done
