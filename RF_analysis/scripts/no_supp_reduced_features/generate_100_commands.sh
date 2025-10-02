add736179f3f3bdb5db99a4fda41bfb8e9575182
Margaret Hilliard <53877776+margarethilliard@users.noreply.github.com>
2025-10-02 15:40:28 -0400

    Delete RF_analysis/scripts directory

--- RF_analysis/scripts/no_supp_reduced_features/generate_100_commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for seed in $(cat random_seeds.txt) 
-do
-echo "mkdir -p /home/hilliard/no_supp_best_model_reduced_features/output_${seed} && singularity exec --no-home --bind /home/hilliard/no_supp_best_model_reduced_features/:/data /quobyte/dglemaygrp/aoliver/software/nutrition_tools.sif dietML --subject_identifier subject_id --label plasma_b12 --type regression --model rf --ncores 6 --metric rmse --cor_level 0.95 --train_split 0.75 --tune_length 80 --tune_time 2 --tune_stop 10 --folds 5 --seed ${seed} /data/dietML_metadata.csv /data/output_${seed}/ml_results/" >> 100_best_commands.txt
-done
--- RF_analysis/scripts/no_supps/dietML_watch_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-total_runs=$(column --table -s',' output_*/ml_results/ml_results.csv | grep "^rsq" | awk '{print $3"\t"$6}' | sort -k1,1nr | wc -l)
-
-echo "Total Runs Completed: " ${total_runs}
-
-column --table -s',' output_*/ml_results/ml_results.csv | \
-grep "^rsq" | awk '{print $3"\t"$6}' | grep -v "e" | sort -k1,1nr | head
-
