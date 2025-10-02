d3613f12f4fdc54c01b110e5ae317ee0f499145c
Margaret Hilliard <53877776+margarethilliard@users.noreply.github.com>
2025-10-02 15:43:49 -0400

    Add files via upload

---
+++ RF_analysis/scripts/full_data/generate_100_commands.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for seed in $(cat random_seeds.txt) 
+do
+echo "mkdir -p /quobyte/dglemaygrp/mhilliard/FL100/dietML/full_data/output_${seed} && singularity exec --no-home --bind /quobyte/dglemaygrp/mhilliard/FL100/dietML/full_data/:/data /quobyte/dglemaygrp/aoliver/software/nutrition_tools.sif dietML --subject_identifier subject_id --label plasma_b12 --type regression --model rf --ncores 6 --metric rmse --cor_level 0.95 --train_split 0.75 --tune_length 80 --tune_time 2 --tune_stop 10 --folds 5 --seed ${seed} /data/dietML_metadata.csv /data/output_${seed}/ml_results/" >> 100_best_commands.txt
+done
